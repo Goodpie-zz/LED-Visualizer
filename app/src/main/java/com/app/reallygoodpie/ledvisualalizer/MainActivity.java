ebb4f6ac6925a2b28516ed6ba7aba00eb0b02d01
GoodPie <Brandynbb96@gmail.com>
2017-02-27 14:11:00 +0800

    Changed the color selector

--- app/src/main/java/com/app/reallygoodpie/ledvisualalizer/MainActivity.java
+++ app/src/main/java/com/app/reallygoodpie/ledvisualalizer/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.Toast;
 
 import com.app.reallygoodpie.ledvisualalizer.adapters.ColorGridAdapter;
 import com.app.reallygoodpie.ledvisualalizer.models.ColorGridModel;
-import com.thebluealliance.spectrum.SpectrumDialog;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
 
 import java.util.Map;
 
@@ -33,9 +33,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private GridView gridView;
     private CheckBox brushChecKBox;
     private Button colorSelectButton, saveButton, fillButton;
+    private ColorPicker mColorPicker;
 
     private Context mContext;
-    private SpectrumDialog.Builder spectrumBuilder;
     private ColorGridAdapter mAdapter;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +44,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mContext = getApplicationContext();
 
+        mColorPicker = new ColorPicker(MainActivity.this, 33, 159, 243);
+
         // Initialize the grid
         currentGrid = new ColorGridModel();
         currentGrid.init(ContextCompat.getColor(getApplicationContext(), R.color.md_blue_500)); // Default color to blue
@@ -51,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Get UI elements
         gridView = (GridView) findViewById(R.id.color_gridview);
         brushChecKBox = (CheckBox) findViewById(R.id.brush_checkbox);
+        brushChecKBox.setChecked(true);
 
         colorSelectButton = (Button) findViewById(R.id.brush_color_button);
         colorSelectButton.setOnClickListener(this);
@@ -58,18 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fillButton = (Button) findViewById(R.id.fill_button);
         fillButton.setOnClickListener(this);
 
-        // Map the colors to their values
-        colorMap = initializeColorMap();
-
         // Set the default color to blue
         currentGlobalColor = ContextCompat.getColor(mContext, R.color.md_green_500);
-
-        // Initialize the spectrum color dialog
-        spectrumBuilder = new SpectrumDialog.Builder(getApplicationContext())
-                .setColors(R.array.main_colors)
-                .setDismissOnColorSelected(true)
-                .setOutlineWidth(2);
-
 
         // Initialize the grid
         mAdapter = new ColorGridAdapter(getApplicationContext(), currentGrid);
@@ -88,17 +81,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (!isPainting) {
                     // Onclick listener to change the currently selected block
                     // Set the currently selected color
-                    spectrumBuilder
-                        .setSelectedColorRes(colorMap.get(currentColor))
-                        .setOnColorSelectedListener(new SpectrumDialog.OnColorSelectedListener() {
-                            @Override
-                            public void onColorSelected(boolean colorSelected, @ColorInt int color) {
-                                if (colorSelected) {
-                                    currentGrid.setColor(color, i);
-                                    mAdapter.notifyDataSetInvalidated();
-                                }
-                            }
-                         }).build().show(getSupportFragmentManager(), "color_chooser_fragment");
+                    mColorPicker.show();
+                    Button okColorSelection = (Button) mColorPicker.findViewById(R.id.okColorButton);
+                    okColorSelection.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            currentGlobalColor = mColorPicker.getColor();
+                            currentGrid.setColor(currentGlobalColor, i);
+                            mColorPicker.dismiss();
+                        }
+                    });
                 }
                 else
                 {
@@ -119,14 +111,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             // Allow the user to select another color for the global color
             case R.id.brush_color_button:
-                spectrumBuilder
-                    .setSelectedColorRes(colorMap.get(currentGlobalColor))
-                    .setOnColorSelectedListener(new SpectrumDialog.OnColorSelectedListener() {
-                        @Override
-                        public void onColorSelected(boolean positiveResult, @ColorInt int color) {
-                            currentGlobalColor = color;
-                        }
-                    }).build().show(getSupportFragmentManager(), "color_chooser_fragment");
+                mColorPicker.show();
+                Button okColorSelection = (Button) mColorPicker.findViewById(R.id.okColorButton);
+                okColorSelection.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        currentGlobalColor = mColorPicker.getColor();
+                        mColorPicker.dismiss();
+                    }
+                });
                 break;
             // Fill the grid with one color
             case R.id.fill_button:
@@ -134,40 +127,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mAdapter.notifyDataSetChanged();
                 break;
         }
-    }
-
-    /**
-     * Initialize color map for SpectrumDialog
-     * Very dirty way of handling it however there doesn't seem to be any other option for binding
-     * the color int to the actual color int (?)
-     *
-     * @return  Map of colors
-     */
-    private Map<Integer, Integer> initializeColorMap()
-    {
-        final Map<Integer, Integer> colorMap = new ArrayMap<>();
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_white), R.color.md_white);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_red_500), R.color.md_red_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_pink_500), R.color.md_pink_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_purple_500), R.color.md_purple_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_deep_purple_500), R.color.md_deep_purple_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_indigo_500), R.color.md_indigo_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_blue_500), R.color.md_blue_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_light_blue_500), R.color.md_light_blue_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_cyan_500), R.color.md_cyan_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_teal_500), R.color.md_teal_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_green_500), R.color.md_green_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_light_green_500), R.color.md_light_green_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_lime_500), R.color.md_lime_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_yellow_500), R.color.md_yellow_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_amber_500), R.color.md_amber_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_orange_500), R.color.md_orange_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_deep_orange_500), R.color.md_deep_orange_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_brown_500), R.color.md_brown_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_grey_500), R.color.md_grey_500);
-        colorMap.put(ContextCompat.getColor(mContext, R.color.md_blue_grey_500), R.color.md_blue_grey_500);
-
-        return colorMap;
     }
 
 
